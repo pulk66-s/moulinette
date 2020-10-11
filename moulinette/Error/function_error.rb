3a72b98af41d7d1bae66ab66bfdc4df387a9d591
pulk66-s <hugo.marrasse@epitech.eu>
2020-10-11 20:22:50 +0200

    add of lines column verification

--- moulinette/Error/function_error.rb
+++ moulinette/Error/function_error.rb
@@ -1,7 +1,25 @@
 class Function_error
     def initialize(lines)
         verif_nb_lines(lines);
+        verif_column(lines);
     end
+
+    def verif_column(lines)
+        len = lines.length;
+        line_len = 0;
+        i = 0;
+        j = 0;
+        while (i < len)
+            j = 0;
+            if (lines[i] != nil)
+                if (lines[i].length > 80)
+                    puts "F3 error in line #{i}: you have 80+ column line"
+                end
+            end
+            i += 1;
+        end
+    end
+
     def verif_nb_lines(lines)
         len = lines.length;
         i = 0;
